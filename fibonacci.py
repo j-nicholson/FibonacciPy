7507defa06ee10fb131528bc63e6c12c6f79e901
j-nicholson <johnfnicholson123@gmail.com>
2018-01-16 17:17:06 -0500

    Added timing mechanism and bounds checking

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,18 @@
-# Python Program for calculating the nth Fibonacci numbers
-# f[n] = f[n - 1] + f[n - 2]
+""" Python Program for calculating the nth Fibonacci number
+f[n] = f[n - 1] + f[n - 2] """
 
+import time
+
+""" Recursive Fib calcuation """
 def fibonacciR(n):
-    if n == 0:
-        return 0
-    elif n > 0 and n <= 2:
+    if n <= 2:
         return 1
     else:
         return fibonacciR(n - 1) + fibonacciR(n - 2)
 
+""" Iterative Fib calculation """
 def fibonacciI(n):
-    if n == 0:
-        return 0
-    elif n > 0 and n <= 2:
+    if n <= 2:
         return 1
     else:
         newValue = 0 # f[n]
@@ -27,8 +27,20 @@ def fibonacciI(n):
     return newValue
 
 if __name__ == "__main__":
+    #import timeit
 
-    n = input('Enter the number of Fibonacci Numbers to calculate: ')
+    print('* * * Fibonacci Printer * * *\n')
 
-    print('Recursive Solution: ' + str(fibonacciR(int(n))))
-    print('Iterative Solution: ' + str(fibonacciI(int(n))))
+    n = int(input('Which Fibonacci number would you like to see?: '))
+    if n > 0 and n < 46:
+        start = time.time()
+        print('\nFibonacci number ' + str(n) + ' is: ' + str(fibonacciR(n)) + '\n')
+        end = time.time()
+        totalTime = end - start
+
+        #t = timeit.timeit("fibonacciR(" + str(n) + ")", setup="from __main__ import fibonacciR", number=1)
+
+        print('This calculation required ' + '%.3f' % totalTime + ' seconds.\n')
+        #print('Iterative Solution: ' + str(fibonacciI(int(n))))
+    else:
+        print('Error: entry must be from 1 to 45 inclusive.\n')
